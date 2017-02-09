4a3b137f847e83003b15c7cde7eb5061b53ed2d2
asapelkin <asapelkin@github.com>
2017-02-09 17:38:53 +0300

    реализован основной функционал

--- bot.py
+++ bot.py
@@ -5,9 +5,11 @@ import lastfm
 # import os.path
 
 bot = telebot.TeleBot(config.token)
-usersArrays = dict()
-
+user_out_dict = dict() # словарь, ставящий в соответствие каждому пользователю полученный массив исполнителей/треков для дальнейшей работы
+user_inp_dict = dict() # словарь, ставящий в соответствие каждому пользователю исходный массив исполнителей/треков для дальнейшей работы
+waitAddFlag = dict() # флаги ожидания добавления нового искомого
 stepNumItems = 10 # количество
+
 
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message):
@@ -21,19 +23,31 @@ def handle_start_help(message):
     """
     bot.send_message(message.chat.id, helpText)
 
+
 @bot.message_handler(content_types=["text"])
 def text_handler(message): # обработчик текстовых сообщений
+
+    #if waitAddFlag[message.chat.id]: # пришло добавление в исходный массив
+
+
     textMessage = message.text
     chat_id = message.chat.id
     print("Получено сообщение = " + textMessage)
     keyboard = types.InlineKeyboardMarkup()
+
+    if len(user_inp_dict) == 0:
+        user_inp_dict[message.chat.id] = [textMessage]
+    else:
+        user_inp_dict[message.chat.id].append(textMessage)
+
     artists_button = types.InlineKeyboardButton(text="Найти похожих исполнителей", callback_data="show artist " + textMessage)
     tracks_button = types.InlineKeyboardButton(text="Найти похожие треки", callback_data="show track " + textMessage)
-    add_button = types.InlineKeyboardButton(text="Добавить еще @todo", callback_data="add " + textMessage)
+    add_button = types.InlineKeyboardButton(text="Добавить еще один трек или исполнителя", callback_data="add " + textMessage)
     keyboard.add(artists_button)
     keyboard.add(tracks_button)
     keyboard.add(add_button)
     bot.send_message(message.chat.id, "Выберите действие:", reply_markup=keyboard)
+
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
@@ -47,12 +61,19 @@ def callback_inline(call):
     if action == "show":
         showAction(call)
     elif action == "add":
-        print("add")
+        addAction(call)
     elif action == "next":
         sendNext(call)
 
 
+def addAction(call):
+    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Введите еще одну композицию или исполнителя.")
+    waitAddFlag[call.message.chat.id] = True
+
+
 def showAction(call):
+    print("user_inp_dict")
+    print(user_inp_dict)
     textMessArray = call.data.split(' ')
     method = ""
 
@@ -81,9 +102,9 @@ def showAction(call):
     print("track_name = " + track_name)
 
     if method == "artist":
-        usersArrays[call.message.chat.id] = lastfm.getSimilarArtists(config.lastFmKey, artist_name, track_name)
+        user_out_dict[call.message.chat.id] = lastfm.getSimilarArtists(config.lastFmKey, artist_name, track_name)
     elif method == "track":
-        usersArrays[call.message.chat.id] = lastfm.getSimilarTracks(config.lastFmKey, artist_name, track_name)
+        user_out_dict[call.message.chat.id] = lastfm.getSimilarTracks(config.lastFmKey, artist_name, track_name)
     else:
         return
     call.data = "next " + method
@@ -96,7 +117,7 @@ def sendNext(call):
     if len(textMessArray) >= 2:
         method = textMessArray[1]
 
-    num_items = min(len(usersArrays[call.message.chat.id]), stepNumItems)
+    num_items = min(len(user_out_dict[call.message.chat.id]), stepNumItems)
 
     print("num_items = " + str(num_items))
 
@@ -115,35 +136,36 @@ def sendNext(call):
 
     if method == "artist":
         for i in range(0, num_items):
-            print(usersArrays[call.message.chat.id][i].name)
-            print(usersArrays[call.message.chat.id][i].image)
-            print(usersArrays[call.message.chat.id][i].url)
+            print(user_out_dict[call.message.chat.id][i].name)
+            print(user_out_dict[call.message.chat.id][i].image)
+            print(user_out_dict[call.message.chat.id][i].url)
             print()
-            mess2send = usersArrays[call.message.chat.id][i].name + " \n" + usersArrays[call.message.chat.id][i].url
+            mess2send = user_out_dict[call.message.chat.id][i].name + " \n" + user_out_dict[call.message.chat.id][i].url
             bot.send_message(call.message.chat.id, mess2send, disable_web_page_preview=True)
             ## bot.send_photo(call.message.chat.id, usersArrays[call.message.chat.id][i].image)
     elif method == "track":
         for i in range(0, num_items):
-            print(usersArrays[call.message.chat.id][i].artist)
-            print(usersArrays[call.message.chat.id][i].name)
-            print(usersArrays[call.message.chat.id][i].image)
-            print(usersArrays[call.message.chat.id][i].url)
+            print(user_out_dict[call.message.chat.id][i].artist)
+            print(user_out_dict[call.message.chat.id][i].name)
+            print(user_out_dict[call.message.chat.id][i].image)
+            print(user_out_dict[call.message.chat.id][i].url)
             print()
-            mess2send = usersArrays[call.message.chat.id][i].artist + " - " + usersArrays[call.message.chat.id][i].name + " \n" + \
-                        usersArrays[call.message.chat.id][i].url
+            mess2send = user_out_dict[call.message.chat.id][i].artist + " - " + user_out_dict[call.message.chat.id][i].name + " \n" + \
+                        user_out_dict[call.message.chat.id][i].url
             bot.send_message(call.message.chat.id, mess2send, disable_web_page_preview=True)
             ## bot.send_photo(call.message.chat.id, usersArrays[call.message.chat.id][i].image)
     else:
         return
 
-    usersArrays[call.message.chat.id] = usersArrays[call.message.chat.id][num_items:]
+    user_out_dict[call.message.chat.id] = user_out_dict[call.message.chat.id][num_items:]
 
-    if len(usersArrays[call.message.chat.id]) > 0:
+    if len(user_out_dict[call.message.chat.id]) > 0:
         keyboard = types.InlineKeyboardMarkup()
         next_button = types.InlineKeyboardButton(text="Показать еще " + str(stepNumItems),
                                                  callback_data="next " + method)
         keyboard.add(next_button)
         bot.send_message(call.message.chat.id, "...", reply_markup=keyboard)
+
 
 if __name__ == '__main__':
      bot.polling(none_stop=True)
